1b06a52aaeaca0befed6a4dca0ca22532cb861e7
DELTA\BARRY.KE <barry.ke@deltaww.com>
2021-02-25 17:43:43 +0800

    hello word

---
+++ hello-world/src/AppTT.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+
+function AppClick(props) {
+    
+    const colors = {
+        Sea: '#a2ccb6',
+        Sand: '#fceeb5',
+        Peach: '#ee786e',
+      }
+
+    const [count, setCount] = useState(0);
+    const [getName, setName] = useState(false);  
+    
+
+    function changeTextColor_Over(e) {
+        e.target.style.color = colors.Sand;        
+    }
+    
+    function changeTextColor_Leave(e) {
+        e.target.style.color = 'black';
+    }
+
+    function changeBackground(e) {
+        e.target.style.background = 'red';
+    }
+
+
+
+    return (                
+        //<Button onClick={() => setButtonText("newText")}>{buttonText}</Button>        
+        //<button> {props.name} </button>
+
+        //<button onClick={() => {setCount(count+1); setName(props.name)}}> {getName? 'Active' : props.name} </button>
+        <div>
+            <p>You clicked {count} times</p>            
+            <p><button onClick={() => {setCount(count+1); setName(props.name)}}> {getName? count : props.name} </button> </p>
+            <h1 onMouseOver={changeTextColor_Over}>Hello, world!</h1>            
+            <button onMouseOver={changeBackground}>Hover over me!</button>
+        </div>
+
+    );
+}
+
+
+export default AppClick;
+
+
